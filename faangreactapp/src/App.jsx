44a8669a203a82aa9f9aac836626c194689d98c0
edupoly <info.edupoly@gmail.com>
2025-09-03 12:49:09 +0530

    comp-communication

--- faangreactapp/src/App.jsx
+++ faangreactapp/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import Counter from "./Counter";
 
 function App() {
+  function naistam(ar) {
+    alert("Naistam babu" + ar);
+  }
   return (
     <div className="border border-3 border-info p-2 m-2">
       <h2>App</h2>
-      <Counter iv={100}></Counter>
-      <Counter iv={200}></Counter>
+      <Counter iv={100} s={10} n={naistam}></Counter>
+      <Counter iv={200} s={5} n={naistam}></Counter>
     </div>
   );
 }
--- faangreactapp/src/Counter.jsx
+++ faangreactapp/src/Counter.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Counter(props) {
   console.log(props);
-  let [count, setCount] = React.useState(0);
+  let [count, setCount] = React.useState(props.iv);
   function inc() {
-    setCount(count + 1);
+    setCount(count + props.s);
   }
   function dec() {
-    setCount(count - 1);
+    setCount(count - props.s);
   }
   return (
     <div className="border border-3 border-danger p-2 m-2">
@@ -25,6 +25,13 @@ function Counter(props) {
         }}
       >
         Decrement
+      </button>
+      <button
+        onClick={() => {
+          props.n(count);
+        }}
+      >
+        Call your parents
       </button>
     </div>
   );
