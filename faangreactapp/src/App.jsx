da714833678fb273f5d278f40c3af753ecec8666
edupoly <info.edupoly@gmail.com>
2025-09-16 12:16:27 +0530

    reduxtoolkit-todolist

--- faangreactapp/src/App.jsx
+++ faangreactapp/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Counter from "./features/counter/Counter";
+import Todolist from "./features/todolist/Todolist";
 
 function App() {
   return (
     <div className="border border-2 p-2 m-2 border-success">
       <h2>Hello Edupoly</h2>
+      <Todolist></Todolist>
       <Counter></Counter>
     </div>
   );
--- faangreactapp/src/features/todolist/Todolist.jsx
+++ faangreactapp/src/features/todolist/Todolist.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Todolist() {
   return (
-    <div className="border border-4 p-2 m-2 border-danger">
+    <div className="border border-4 p-2 m-2 border-info">
       <h1>Todolist</h1>
     </div>
   );
