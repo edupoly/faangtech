04b1d8eff29e4d8b94bd37a60bda2c861c19a795
edupoly <info.edupoly@gmail.com>
2025-09-16 11:47:23 +0530

    reduxtoolkit

--- faangreactapp/src/App.jsx
+++ faangreactapp/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
     <div className="border border-2 p-2 m-2 border-success">
       <h2>Hello Edupoly</h2>
+      <Counter></Counter>
     </div>
   );
 }
---
+++ faangreactapp/src/features/counter/Counter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment } from "./counterSlice";
+
+function Counter() {
+  var { count } = useSelector((state) => state.cntR);
+  var dispatch = useDispatch();
+  return (
+    <div className="border border-4 p-2 m-2 border-danger">
+      <h1>Counter:{count}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
