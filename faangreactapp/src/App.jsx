df3fb8fad77c38da2fc42c8c7ca67065fb80bfe0
edupoly <info.edupoly@gmail.com>
2025-09-17 11:43:40 +0530

    redux-query-api-calls

--- faangreactapp/src/App.jsx
+++ faangreactapp/src/App.jsx
@@ -1,13 +1,25 @@
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 import Counter from "./features/counter/Counter";
+import Products from "./features/products/Products";
 import Todolist from "./features/todolist/Todolist";
 
 function App() {
   return (
     <div className="border border-2 p-2 m-2 border-success">
       <h2>Hello Edupoly</h2>
-      <Todolist></Todolist>
-      <Counter></Counter>
+      <ul className="d-flex flex-wrap justify-content-evenly list-unstyled">
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/todolist">Todolist</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+      <Outlet></Outlet>
     </div>
   );
 }
--- faangreactapp/src/main.jsx
+++ faangreactapp/src/main.jsx
@@ -3,8 +3,33 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "../src/app/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Counter from "./features/counter/Counter.jsx";
+import Todolist from "./features/todolist/Todolist.jsx";
+import Products from "./features/products/Products.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/counter",
+        element: <Counter></Counter>,
+      },
+      {
+        path: "/todolist",
+        element: <Todolist></Todolist>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+    ],
+  },
+]);
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </Provider>
 );
